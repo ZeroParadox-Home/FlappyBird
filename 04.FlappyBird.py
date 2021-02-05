aa6e9111fd1269b4b4d18d42e379d28e5d0d5058
NABEGHEHA <74573961+NABEGHEHACOM@users.noreply.github.com>
2021-02-05 15:11:27 +0330

    Update 04.FlappyBird.py

--- 04.FlappyBird.py
+++ 04.FlappyBird.py
@@ -16,15 +16,9 @@ import sys
 display_width = 576
 display_height = 1024
 floor_x = 0
-gravity = 0.25
-bird_movment = 0
 background_image = pygame.transform.scale2x(pygame.image.load('assets/img/bg2.png'))
 floor_image = pygame.transform.scale2x(pygame.image.load('assets/img/floor.png'))
-bird_image = pygame.transform.scale2x(pygame.image.load('assets/img/red_bird_mid_flap.png'))
 
-# ---------- #
-
-bird_image_rect = bird_image.get_rect(center=(100, 520))
 # GAME DISPLAY
 main_screen = pygame.display.set_mode((display_width, display_height))
 
@@ -44,11 +38,6 @@ while True:
             sys.exit()
     # DISPLAY BG2.PNG        
     main_screen.blit(background_image, (0, 0))
-    # DISPLAY BIRD IMAGE
-    main_screen.blit(bird_image, bird_image_rect)
-    # FLOOR GRAVITY AND BIRD MOVEMENT
-    bird_movment += gravity
-    bird_image_rect.centery += bird_movment
     # DISPLAY FLOOR.PNG
     floor_x -= 1
     main_screen.blit(floor_image, (floor_x, 900))
