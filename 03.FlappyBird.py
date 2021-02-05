5459bc7341f53a3697331e87cfc0e54679a6dd61
NABEGHEHA <74573961+NABEGHEHACOM@users.noreply.github.com>
2021-02-05 15:09:22 +0330

    Update 03.FlappyBird.py

--- 03.FlappyBird.py
+++ 03.FlappyBird.py
@@ -1,3 +1,4 @@
+  
 # NABEGHEHA.COM
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -#
 # Github       : https://github.com/NABEGHEHACOM/FlappyBird/
@@ -15,9 +16,8 @@ import sys
 # ALL VARIABLE
 display_width = 576
 display_height = 1024
-floor_x = 0
-background_image = pygame.transform.scale2x(pygame.image.load('assets/img/bg2.png'))
-floor_image = pygame.transform.scale2x(pygame.image.load('assets/img/floor.png'))
+background_image = pygame.image.load('assets/img/bg2.png')
+background_image = pygame.transform.scale2x(background_image)
 
 # GAME DISPLAY
 main_screen = pygame.display.set_mode((display_width, display_height))
@@ -36,15 +36,7 @@ while True:
             pygame.quit()
             # TERMINATE PROGRAM
             sys.exit()
-    # DISPLAY BG2.PNG        
     main_screen.blit(background_image, (0, 0))
-    # DISPLAY FLOOR.PNG
-    floor_x -= 1
-    main_screen.blit(floor_image, (floor_x, 900))
-    main_screen.blit(floor_image, (floor_x + 576, 900))
-    if floor_x <= -576:
-        floor_x = 0
-    
     pygame.display.update()
     # SET GAME SPEED
-    clock.tick(90) 
+    clock.tick(90)
